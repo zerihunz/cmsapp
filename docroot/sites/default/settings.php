93bc788335eabeff00affc16b5b973614ad70150
ZTEGEGN <zerihun.tegegn@cgifederal.com>
2018-10-09 15:49:46 -0400

    Move composer.json and vendor outside of docroot.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -768,19 +768,4 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-
-// Initialize install_profile to the Drupal distribution name detected by the Acquia Cloud installation process.
-if (class_exists('Drupal') && defined('Drupal::CORE_COMPATIBILITY') && Drupal::CORE_COMPATIBILITY == '8.x') {
- if (!isset($settings['install_profile'])) {
-    $settings['install_profile'] = 'lightning';
-  }
-}
-
-
-// On Acquia Cloud, this include file configures Drupal to use the correct
-// database in each site environment (Dev, Stage, or Prod). To use this
-// settings.php for development on your local workstation, set $db_url
-// (Drupal 5 or 6) or $databases (Drupal 7 or 8) as described in comments above.
-if (file_exists('/var/www/site-php')) {
-  require('/var/www/site-php/medicaid/medicaid-settings.inc');
-}
+$config_directories['sync'] = '../config/sync';
